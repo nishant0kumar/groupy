d51596f15d85e7dd7319818894b8a864331f48dc
Nishant Kumar <nishant51121@gmail.com>
2025-01-16 22:54:39 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserLinks from './components/UserLinks';
 import CategoryLinks from './components/CategoryLinks';
-import './styles/components.css';
 import Navigation from './components/Navigation';
 function App() {
     return (
